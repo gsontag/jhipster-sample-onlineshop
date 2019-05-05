2642eaa22577b3823c7b285128f534f26726b908
gsontag <gsontag@users.noreply.github.com>
2019-05-05 12:56:48 +0200

    Revert "Add entities using the JDL model `onlineshop`"

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,26 +4,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
-            {
-                path: 'category',
-                loadChildren: './category/category.module#OnlineShopCategoryModule'
-            },
-            {
-                path: 'product',
-                loadChildren: './product/product.module#OnlineShopProductModule'
-            },
-            {
-                path: 'customer',
-                loadChildren: './customer/customer.module#OnlineShopCustomerModule'
-            },
-            {
-                path: 'address',
-                loadChildren: './address/address.module#OnlineShopAddressModule'
-            },
-            {
-                path: 'wish-list',
-                loadChildren: './wish-list/wish-list.module#OnlineShopWishListModule'
-            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
